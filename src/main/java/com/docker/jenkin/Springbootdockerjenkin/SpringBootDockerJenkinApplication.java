ee2021a80c6f7e61839dc14fb833aa465f3bbdcc
mahaveer <dev2.synarion@gmail.com>
2022-11-20 15:52:37 +0530

    first commit

---
+++ src/main/java/com/docker/jenkin/Springbootdockerjenkin/SpringBootDockerJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.docker.jenkin.Springbootdockerjenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/jenkin/Springbootdockerjenkin/SpringBootDockerJenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.jenkin.Springbootdockerjenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerJenkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
