edc045aab1120c4465ad210a99e1c0540c075514
mahaveer <dev2.synarion@gmail.com>
2022-11-27 13:33:27 +0530

    Home  message

--- src/main/java/com/docker/jenkin/Springbootdockerjenkin/WelcomeController.java
+++ src/main/java/com/docker/jenkin/Springbootdockerjenkin/WelcomeController.java
@@ -11,4 +11,8 @@ public class WelcomeController {
     public String getWelcomeMessage(){
         return "Welcome to docker with jenkins.";
     }
+    @GetMapping("/home")
+    public String getHomeMessage(){
+        return "Welcome to Home page of docker with jenkins.";
+    }
 }
